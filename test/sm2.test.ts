55569c55235c0a82525b142ea9a4f8a2669654ce
haotianlong <haotianlong@tencent.com>
2024-07-09 15:14:34 +0800

    fix(asn1): correct the implementation of the long version of L encoding in asn.1

--- test/sm2.test.ts
+++ test/sm2.test.ts
@@ -96,17 +96,38 @@ describe('sm2: encrypt and decrypt data', () => {
 
 
 describe('sm2: encrypt and decrypt data using asn1 encoding', () => {
-    it('decrypted data should be correct', () => {
+    it('decrypted data should be correct (non asn.1)', () => {
+        const input = 'aabbccdd'
+        const res = sm2.doEncrypt(hexToArray(input), '049812a275eca335e85998eb4030a6cc9e88a098010bdbfc134b26e29c43253439d3821ef18e69e0813bcc55eee7dc9163f1edb81ad2032b20cbdf1408897faaac', 1, {
+            asn1: false,
+        })
+        // console.log('res', res)
+        const dec = sm2.doDecrypt(res, '75b25a5d6101013e9be25816f81cf1f64bf78ea8383b32d61f5b26e6f1429e70', 1, {
+            output: 'array',
+            asn1: false,
+        })
+        expect(arrayToHex([...dec])).toBe(input)
+    })
+    it('decrypted data should be correct (asn.1)', () => {
         const input = 'aabbccdd'
         const res = sm2.doEncrypt(hexToArray(input), '049812a275eca335e85998eb4030a6cc9e88a098010bdbfc134b26e29c43253439d3821ef18e69e0813bcc55eee7dc9163f1edb81ad2032b20cbdf1408897faaac', 1, {
             asn1: true,
         })
-        // console.log('res', res)
         const dec = sm2.doDecrypt(res, '75b25a5d6101013e9be25816f81cf1f64bf78ea8383b32d61f5b26e6f1429e70', 1, {
             output: 'array',
             asn1: true,
         })
         expect(arrayToHex([...dec])).toBe(input)
+        const input2 = 'xxxxx{"xx": "111","t": "1"}'
+        const res2 = sm2.doEncrypt(input2, '049812a275eca335e85998eb4030a6cc9e88a098010bdbfc134b26e29c43253439d3821ef18e69e0813bcc55eee7dc9163f1edb81ad2032b20cbdf1408897faaac', 1, {
+            asn1: true,
+        })
+        console.log(res2)
+        const dec2 = sm2.doDecrypt(res2, '75b25a5d6101013e9be25816f81cf1f64bf78ea8383b32d61f5b26e6f1429e70', 1, {
+            output: 'string',
+            asn1: true,
+        })
+        expect(dec2).toBe(input2)
     })
     it('decrypted data should be correct: c1c2c3', () => {
         const input = 'aabbccdd'
